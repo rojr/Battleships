adc5f9ee8f09f28a3a8ffcac3151ddf0375ce495
robmadeyou <robmadeyou@gmail.com>
2015-06-18 22:08:04 +0100

    adding ship additions

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 </div>
 <?php
 	print '<h1>Your board</h1>';
-	print '<div style="width: 500px; position:relative; padding: 0; margin: 0" >';
+	print '<div style="width: 500px; height:500px; position:relative; padding: 0; margin: 0" >';
 	for( $i = 0; $i < 11; $i++ )
 	{
 		if( $i != 0 )
@@ -31,14 +31,37 @@
 			{
 				if( $j != 0 )
 				{
-					print '<div id="' . $i . '-' . $j . '" class="click-cell" style="float:left; width: 40px; height:40px; border: white 1px solid" ></div>';
+					print '<div id="y' . $i . '-' . $j . '" style="float:left; width: 40px; height:40px; border: white 1px solid" ></div>';
 				}
 			}
 		}
 	}
 	print '</div>';
 
-	print '<h1>Enemy ship</h1>';
+	print '<h1>Enemy board</h1>';
+
+	for( $i = 0; $i < 11; $i++ )
+	{
+		if( $i != 0 )
+		{
+			print '<div style="float:left; width: 40px; height: 40px; border: white 1px solid"> ' . $i . ' </div>';
+		}
+		for( $j = 0; $j < 11; $j++ )
+		{
+			if( $i == 0 )
+			{
+				print '<div style="float:left; width:40px; height:40px; border: white 1px solid" >' . $j . '</div>';
+			}
+			else
+			{
+				if( $j != 0 )
+				{
+					print '<div id="e' . $i . '-' . $j . '" class="click-cell" style="float:left; width: 40px; height:40px; border: white 1px solid" ></div>';
+				}
+			}
+		}
+	}
+	print '</div>';
 
 ?>
 </body>
--- socket.js
+++ socket.js
@@ -1,9 +1,24 @@
 var io = require( './socket.io/' ).listen( 1337 );
 
-var user1 = 0;
-var user2 = 0;
+var user1 = {
+	id : 0,
+	ready : false
+};
+var user2 = {
+	id : 0,
+	ready : false
+};
 
-var board = [10][10];
+/*
+
+	0 empty
+	1 miss hit
+	2 ship safe
+	3 ship hit
+
+ */
+var board1 = [10][10];
+var board2 = [10][10];
 
 io.sockets.on( 'connection', function( socket )
 {
@@ -12,17 +27,33 @@ io.sockets.on( 'connection', function( socket )
 		console.log( data.id );
 		if( user1 != 0 )
 		{
-			user2 = data.id;
+			user2.id = data.id;
 		}
 		else
 		{
-			user1 = data.id;
+			user1.id = data.id;
 		}
 
 		if( user1 != 0 && user2 != 0 )
 		{
 			socket.broadcast.emit( 'ready' );
 			socket.emit( 'ready' );
+		}
+	});
+
+	socket.on( 'logShip', function( data )
+	{
+		for( var i = 0; i < data.entry.length; i++ )
+		{
+			var split = data.entry[ i ].split( "-" );
+			if( data.id == user1.id )
+			{
+				board1[ split[0] ][ split[ 1] ] = 2;
+			}
+			else
+			{
+				board2[ split[0] ][ split[ 1] ] = 2;
+			}
 		}
 	});
 
