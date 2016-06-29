28ed8bb1019150555ca2a5a8b3e6e4d3a2d15fbb
robmadeyou <robmadeyou@gmail.com>
2016-06-29 09:56:00 +0100

    made boards better

--- index.php
+++ index.php
@@ -46,29 +46,7 @@
 <?php
 	print '<h1>Your board</h1>';
 	print '<h1 id="gameStatus"></h1>';
-	print '<div style="width: 500px; height:500px; position:relative; padding: 0; margin: 0" >';
-	for( $i = 0; $i < 11; $i++ )
-	{
-		if( $i != 0 )
-		{
-			print '<div style="float:left; width: 40px; height: 40px; border: white 1px solid"> ' . $i . ' </div>';
-		}
-		for( $j = 0; $j < 11; $j++ )
-		{
-			if( $i == 0 )
-			{
-				print '<div style="float:left; width:40px; height:40px; border: white 1px solid" >' . $j . '</div>';
-			}
-			else
-			{
-				if( $j != 0 )
-				{
-					print '<div id="y' .  ( $i - 1 ) . '-' . ( $j - 1 ) . '" class="personal-ships" style="float:left; width: 40px; height:40px; border: white 1px solid" ></div>';
-				}
-			}
-		}
-	}
-	print '</div>';
+	printBoard('personal-ships');
 
 	?>
 	<div class="ship-selection">
@@ -79,32 +57,35 @@
 	</div>
 	<?php
 	print '<h1>Enemy board</h1>';
+	printBoard( 'click-cell' );
 
-	print '<div style="width: 500px; height:500px; position:relative; padding: 0; margin: 0" >';
-
-	for( $i = 0; $i < 11; $i++ )
+	function printBoard( $class = '' )
 	{
-		if( $i != 0 )
+		print '<div style="width: 500px; height:500px; position:relative; padding: 0; margin: 0" >';
+
+		for( $i = 0; $i < 11; $i++ )
 		{
-			print '<div style="float:left; width: 40px; height: 40px; border: white 1px solid"> ' . $i . ' </div>';
-		}
-		for( $j = 0; $j < 11; $j++ )
-		{
-			if( $i == 0 )
+			if( $i != 0 )
 			{
-				print '<div style="float:left; width:40px; height:40px; border: white 1px solid" >' . $j . '</div>';
+				print '<div style="float:left; width: 40px; height: 40px; border: white 1px solid"> ' . $i . ' </div>';
 			}
-			else
+			for( $j = 0; $j < 11; $j++ )
 			{
-				if( $j != 0 )
+				if( $i == 0 )
 				{
-					print '<div id="e' . ( $i - 1 ) . '-' . ( $j - 1 ) . '" class="click-cell" style="float:left; width: 40px; height:40px; border: white 1px solid" ></div>';
+					print '<div style="float:left; width:40px; height:40px; border: white 1px solid" >' . $j . '</div>';
+				}
+				else
+				{
+					if( $j != 0 )
+					{
+						print '<div id="e' . ( $i - 1 ) . '-' . ( $j - 1 ) . '" class="' . $class . '" style="float:left; width: 40px; height:40px; border: white 1px solid" ></div>';
+					}
 				}
 			}
 		}
+		print '</div>';
 	}
-	print '</div>';
-
 ?>
 </body>
 </html>
